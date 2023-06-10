6f0741e7a22dd4ff8f158635448701b7fdd6c43f
Joel Hooks <joelhooks@gmail.com>
2023-06-10 15:12:06 -0700

    add copilot

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -19,6 +19,7 @@ return require('packer').startup(function(use)
     requires = { {'nvim-lua/plenary.nvim'} }
   }
   use "rebelot/kanagawa.nvim"
+  use "github/copilot.vim"
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
